3dbd696d8bfc532df70888af87998a1829aa718d
IqbalHossain4 <iqbal-hossain@mail.com>
2024-11-03 22:17:49 +0600

    add event loop

---
+++ event-loop.js
@@ -0,0 +1,9 @@
+setTimeout(function timer() {
+  console.log("Run First Code");
+}, 2000);
+
+console.log("Hello World");
+
+setTimeout(function timerTwo() {
+  console.log("Run Second Code");
+}, 500);
